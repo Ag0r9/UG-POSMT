3b40f7345d0d0edaa95f5861c4ae14b229635b54
Patryk Hubicki <72169376+PatrykHub@users.noreply.github.com>
2023-12-13 08:29:59 +0100

    Adding data (#4)
    * adding data and loading it for translation
    
    * deleting unused import

--- src/main.py
+++ src/main.py
@@ -1,20 +1,48 @@
-from loguru import logger
+from typing import List, Union, Tuple
 from transformers import MarianMTModel, MarianTokenizer
+import pandas as pd
 
 
-def translate(text, model, tokenizer):
+def get_data(data_file: str) -> Tuple[List, List]:
+    data = pd.read_csv(data_file)
+    de, eng = [], []
+    for line in data.iloc[:, 1]:
+        line = line.replace("{'de': ", " ")
+        splitted = line.split(", 'en': ")
+        de.append(splitted[0][1:])
+        eng.append(splitted[1][:-1])
+    return de, eng
+
+
+def translate(
+    text: Union[str, List], model: MarianMTModel, tokenizer: MarianTokenizer
+) -> List:
     translated = model.generate(**tokenizer(text, return_tensors="pt", padding=True))
     return [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 
 
 def main():
+    # Getting a pre-trained model
     model_name = "Helsinki-NLP/opus-mt-en-de"
     tokenizer = MarianTokenizer.from_pretrained(model_name)
-
     model = MarianMTModel.from_pretrained(model_name)
 
-    test = translate("Please, have mercy", model, tokenizer)
-    print(test)
+    # loading data and dividing it to german and english
+    data_file = "../data/data.csv"
+    german_data, english_data = get_data(data_file)
+
+    quantity = 5
+    test = translate(english_data[:quantity], model, tokenizer)
+
+    for x in range(quantity):
+        print(
+            english_data[x]
+            + "\n"
+            + german_data[x]
+            + "\ntranslation:\n"
+            + test[x]
+            + "\n"
+        )
     return
 
 
