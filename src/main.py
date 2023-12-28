ef423fb0d21cbb0846d3b14bead3a7b1474b2093
Ag0r9 <Ag0r@protonmail.com>
2023-12-28 14:16:46 +0100

    chore: my proposition

--- src/main.py
+++ src/main.py
@@ -1,19 +1,10 @@
 from typing import List, Tuple, Union
 
 import pandas as pd
-from transformers import MarianMTModel, MarianTokenizer
 from datasets import load_dataset
-from torch.utils.data import DataLoader, Dataset
-
-def get_data(data_file: str) -> Tuple[List, List]:
-    data = pd.read_csv(data_file)
-    de, eng = [], []
-    for line in data.iloc[:, 1]:
-        line = line.replace("{'de': ", " ")
-        splitted = line.split(", 'en': ")
-        de.append(splitted[0][1:])
-        eng.append(splitted[1][:-1])
-    return de, eng
+from loguru import logger
+from torch.utils.data import DataLoader
+from transformers import MarianMTModel, MarianTokenizer
 
 
 def translate(
@@ -23,45 +14,43 @@ def translate(
     return [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 
 
-def main():
+def get_dataloader() -> DataLoader:
+    dataset = load_dataset("wmt16", "de-en", split="test")
+    dataset_pt = dataset.with_format("torch")["translation"]
+    return DataLoader(dataset_pt, batch_size=64, shuffle=True)
+
+
+def main(debug: bool = False):
     # Getting a pre-trained model
     model_name = "Helsinki-NLP/opus-mt-en-de"
     tokenizer = MarianTokenizer.from_pretrained(model_name)
     model = MarianMTModel.from_pretrained(model_name)
 
-    # loading data and dividing it to german and english
-    data_file = "data/data.csv"
-    german_data, english_data = get_data(data_file)
+    # Getting the data
+    dataloader = get_dataloader()
 
-    quantity = 5
-    test = translate(english_data[:quantity], model, tokenizer)
-
-    for x in range(quantity):
-        print(
-            english_data[x]
-            + "\n"
-            + german_data[x]
-            + "\ntranslation:\n"
-            + test[x]
-            + "\n"
+    if debug:
+        sample = next(iter(dataloader))
+        model_results = translate(sample.get("en"), model, tokenizer)
+        pd.DataFrame(
+            {"en": sample.get("en"), "de": sample.get("de"), "model": model_results}
+        ).to_csv("data/results.csv")
+    else:
+        logger.debbug(
+            "Trzeba zainplementować mierzenie jakości tłumaczenia dla całego datasetu"
         )
-    return
 
 
 if __name__ == "__main__":
-    main()
+    main(debug=True)
 
-    
-class TranslationDataset(Dataset):
-    def __init__(self, data_list):
-        self.data_list = data_list
+# Nie jest nam potrzebne
+# class TranslationDataset(Dataset):
+#     def __init__(self, data_list):
+#         self.data_list = data_list
 
-    def __len__(self):
-        return len(self.data_list)
+#     def __len__(self):
+#         return len(self.data_list)
 
-    def __getitem__(self, index):
-        return self.data_list[index]
-    
-data  = load_dataset("wmt16",'de-en', split="test")
-test_data = TranslationDataset(data['translation'])
-test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+#     def __getitem__(self, index):
+#         return self.data_list[index]
