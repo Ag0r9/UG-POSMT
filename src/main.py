b750f0709a6b86312e885646a5ec367926650a0e
Ag0r9 <51130332+Ag0r9@users.noreply.github.com>
2024-01-07 21:44:21 +0100

    Dataloader and Dataset (#10)
    * Initial commit, data loader
    
    * Czy to idzie w dobrym kierunku?
    
    * added validation data loader
    
    * fix to test
    
    * moved notebook to notebook
    
    * added translationDataset class
    
    * chore: my proposition
    
    * fix: remove typo
    
    * refactor: finishing touch :sparkles:
    
    * refactor: remove not important file
    
    ---------
    
    Co-authored-by: jankabzinski <jankabzinski05@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,53 +1,68 @@
-from typing import List, Tuple, Union
+from typing import Union
 
 import pandas as pd
+from datasets import load_dataset
+from loguru import logger
+from torch.utils.data import DataLoader
 from transformers import MarianMTModel, MarianTokenizer
 
 
-def get_data(data_file: str) -> Tuple[List, List]:
-    data = pd.read_csv(data_file)
-    de, eng = [], []
-    for line in data.iloc[:, 1]:
-        line = line.replace("{'de': ", " ")
-        splitted = line.split(", 'en': ")
-        de.append(splitted[0][1:])
-        eng.append(splitted[1][:-1])
-    return de, eng
-
-
 def translate(
-    text: Union[str, List], model: MarianMTModel, tokenizer: MarianTokenizer
-) -> List:
+    text: Union[str, list[str]], model: MarianMTModel, tokenizer: MarianTokenizer
+) -> list[str]:
     translated = model.generate(**tokenizer(text, return_tensors="pt", padding=True))
     return [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 
 
-def main():
+def get_dataloader() -> DataLoader:
+    return DataLoader(
+        dataset=load_dataset("wmt16", "de-en", split="test").with_format("torch")[
+            "translation"
+        ],
+        batch_size=64,
+        shuffle=True,
+    )
+
+
+def main(
+    input_lang: str = "en",
+    output_lang: str = "de",
+    model_name: str = "Helsinki-NLP/opus-mt-en-de",
+    debug: bool = False,
+):
+    """
+    Main function for translation using a pre-trained model.
+
+    Args:
+        input_lang (str): Input language code (default is "en").
+        output_lang (str): Output language code (default is "de").
+        model_name (str): Name of the pre-trained model (default is "Helsinki-NLP/opus-mt-en-de").
+        debug (bool): Flag indicating whether to run in debug mode (default is False).
+    """
     # Getting a pre-trained model
-    model_name = "Helsinki-NLP/opus-mt-en-de"
-    tokenizer = MarianTokenizer.from_pretrained(model_name)
-    model = MarianMTModel.from_pretrained(model_name)
+    tokenizer: MarianTokenizer = MarianTokenizer.from_pretrained(model_name)
+    model: MarianMTModel = MarianMTModel.from_pretrained(model_name)
 
-    # loading data and dividing it to german and english
-    data_file = "data/data.csv"
-    german_data, english_data = get_data(data_file)
+    # Getting the data
+    dataloader: DataLoader = get_dataloader()
 
-    quantity = 5
-    test = translate(english_data[:quantity], model, tokenizer)
-
-    for x in range(quantity):
-        print(
-            english_data[x]
-            + "\n"
-            + german_data[x]
-            + "\ntranslation:\n"
-            + test[x]
-            + "\n"
+    if debug:
+        sample: dict[str, list[str]] = next(iter(dataloader))
+        model_results: list[str] = translate(
+            sample.get(input_lang, []), model, tokenizer
         )
-    return
+        pd.DataFrame(
+            {
+                input_lang: sample.get(input_lang, []),
+                output_lang: sample.get(output_lang, []),
+                "model": model_results,
+            }
+        ).to_csv("data/results.csv")
+    else:
+        logger.debug(
+            "Trzeba zainplementować mierzenie jakości tłumaczenia dla całego datasetu"
+        )
 
 
 if __name__ == "__main__":
-    main()
-
-# TODO: create DataSet and DataLoader
+    main(debug=True)
