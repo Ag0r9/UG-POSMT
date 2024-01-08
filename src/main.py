ba8b6a5c55131bc6fe8be96d59c32e376e4e3cd7
Ag0r9 <Ag0r@protonmail.com>
2024-01-08 18:58:02 +0100

    feat: typer added

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
+from typing import Optional
+
+import typer
 from torch.utils.data import DataLoader
 from transformers import MarianMTModel, MarianTokenizer
+from loguru import logger
 
 from utils import create_bleu_score, create_sample, get_dataloader
 
+app = typer.Typer()
 
+
+@app.command()
 def main(
-    input_lang: str = "en",
-    output_lang: str = "de",
-    model_name: str = "Helsinki-NLP/opus-mt-en-de",
-    debug: bool = False,
+    input_lang: Optional[str] = "en",
+    output_lang: Optional[str] = "de",
+    model_name: Optional[str] = "Helsinki-NLP/opus-mt-en-de",
+    debug: Optional[bool] = True,
 ):
     """
     Main function for translation using a pre-trained model.
@@ -20,11 +27,14 @@ def main(
         debug (bool): Flag indicating whether to run in debug mode (default is False).
     """
     # Getting a pre-trained model
+    logger.info(f"Settings: {input_lang} -> {output_lang}, {model_name}, debug: {debug}")
     tokenizer: MarianTokenizer = MarianTokenizer.from_pretrained(model_name)
     model: MarianMTModel = MarianMTModel.from_pretrained(model_name)
+    logger.info(f"Model loaded: {model_name}")
 
     # Getting the data
     dataloader: DataLoader = get_dataloader()
+    logger.info("Data loaded")
 
     if debug:
         create_sample(dataloader, input_lang, output_lang, model, tokenizer)
@@ -33,4 +43,4 @@ def main(
 
 
 if __name__ == "__main__":
-    main(debug=False)
+    main()
